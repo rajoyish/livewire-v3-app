f517ca1d0f324403ad461c57c38b0d0e8664cc1a
Rajesh Budhathoki <rajoyish@gmail.com>
2023-09-06 16:13:54 +0545

    Live Update with Live Validation

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -5,7 +5,8 @@
     <form method="POST" wire:submit="save">
         <div>
             <label for="title" class="block font-medium text-sm text-gray-700">Title</label>
-            <input id="title" wire:model="form.title" class="block mt-1 w-full border-gray-300 rounded-md shadow-sm" type="text" />
+            {{-- <input id="title" wire:model="form.title" class="block mt-1 w-full border-gray-300 rounded-md shadow-sm" type="text" /> --}}
+            <input id="title" wire:model.blur="form.title" class="block mt-1 w-full border-gray-300 rounded-md shadow-sm" type="text" />
             @error('form.title')
                 <span class="mt-2 text-sm text-red-600">{{ $message }}</span>
             @enderror
